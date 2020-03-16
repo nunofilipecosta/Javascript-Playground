8d6499568a6597917356d0b2d71e3b9066bb3fd0
Nuno Costa <nunofilipe.costa@gmail.com>
2020-03-16 17:12:28 +0000

    ADD : types, operators

---
+++ src/ES6/App/js/script.js
@@ -0,0 +1,32 @@
+const discount = 49.99;
+let price = 12;
+let name = 'Hiking boots',
+    discounted = false;
+
+showMessage( price  + name + discounted);
+
+console.log(typeof price );
+console.log(++price);
+console.log(price++);
+console.log(price);
+
+console.log( 3 + 2 * 5 );
+console.log( (3 + 2) * 5 );
+console.log( 3 + (2 * 5 ));
+
+price = 1.1 + 1.3 ;
+console.log(price);
+
+console.log("Hello \"World\"");
+let firstName = "Andrea"
+console.log(`Hello
+
+
+${firstName}`);
+
+
+let amount = Number.parseFloat("123.12A325");
+
+let saved = true;
+saved = !saved;
+console.log(saved);
